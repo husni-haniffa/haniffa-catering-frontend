9472915ff180ab44e2e54f233f9e7e5c2f822629
Husni <husniwfayo@gmail.com>
2025-11-28 22:46:40 +0530

    added toast ui

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,12 +1,12 @@
 import { Card } from "@/components/ui/card";
 import { useRouter } from "expo-router";
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home () {
    const router = useRouter()
     return (
-        <SafeAreaView className="grid grid-cols-1 gap-6">
+        <SafeAreaView className="flex-col gap-6">
             <Pressable onPress={() => router.push('/pages/items')}>
                 <Card size="lg" className="items-center text-2xl">
                     <Text>Order</Text>
